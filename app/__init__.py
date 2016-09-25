b7f489f556a18923cc239a38642f34a3f8b4bd24
kelvinkipsang <kelvinkipsang@gmail.com>
2016-09-25 03:52:02 +0300

    adding table User to models.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from config import config
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 bootstrap =Bootstrap()      #create instance as a global var
 
 def create_app(config_name):                            #creating app in function
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
+    db.init_app(app)
     bootstrap.init_app(app)     #connecting extention with app ie bring features to app
 
     from .talks import talks as talks_blueprint
--- config.py
+++ config.py
@@ -3,14 +3,22 @@ import os
 class Config:
     SECRET_KEY =os.environ.get("SECRET_KEY")    # obtaining key from environment
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 class DevelopmentConfig(Config):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "top secret key"   #if it isnt set in environment
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \   #setting paths to dbs
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 class TestingConfig(Config):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 class ProductionConfig(Config):
     pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
 #dictionary placing configs in an easy to use structure
 config = {
     'development': DevelopmentConfig,
@@ -19,3 +27,4 @@ config = {
 
     'default': DevelopmentConfig
 }
+
