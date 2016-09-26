d0e30d7b977dde5da0451ac879d494f74aea583a
kelvinkipsang <kelvinkipsang@gmail.com>
2016-09-26 12:58:42 +0300

    register-ing authentication blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,9 @@ def create_app(config_name):                            #creating app in functio
     db.init_app(app)
     bootstrap.init_app(app)     #connecting extention with app ie bring features to app
 
+    from .auth import auth as auth_blueprints
+    app.register_blueprint(auth_blueprints, url_prefix='/auth')     #prefix-ing all routes defined in bprint as /auth
+
     from .talks import talks as talks_blueprint
     app.register_blueprint(talks_blueprint)         #bring/import the routes from bprint to app
 
