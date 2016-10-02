ad9492f7a27d4f00ac12b7987f589b77000a7f94
kelvinkipsang <kelvinkipsang@gmail.com>
2016-10-02 22:14:19 +0300

    adding tests

--- manage.py
+++ manage.py
@@ -7,7 +7,12 @@ from app.models import User
 app = create_app(os.getenv('FLASK_CONFIG') or 'default') #get environment configuration if set else use default configuration(DevelopmentConfig)
 manager = Manager(app)
 
-
+@manager.command
+def test():
+    from subprocess import call
+    call(['nosetests', '-v',
+          '--with-coverage', '--cover-package=app', '--cover-branches',
+          '--cover-erase', '--cover-html', '--cover-html-dir=cover'])
 
 @manager.command
 def adduser(email, username, admin=False):      #creates new command using flsk script,using name of fnt,checks arg with no default become positional args
